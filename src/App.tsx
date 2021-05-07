db9d1aa1cc3e286835b915f1a0792540082d62f9
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2021-05-07 08:00:07 +0530

    some modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App:React.FC =():JSX.Element=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <h1>Hello, I am vivek</h1>
+      <h2>checkout some of my projects</h2>
+</>
   );
 }
 
