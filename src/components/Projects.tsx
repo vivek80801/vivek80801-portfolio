f2f11d798ce7045c9aaeacd913d0f62ac85154e1
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2021-05-09 06:58:22 +0530

    adding live projects to the portfolio

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,16 +1,81 @@
 import React from "react";
-import projects from "../scss/components/projects.module.scss";
+import project from "../scss/components/projects.module.scss";
+import { projects, IProject } from "../data";
 
 const Projects: React.FC = (): JSX.Element => {
+  const [myProjects, setMyProjects] = React.useState<IProject[]>([]);
+
+  React.useEffect(() => {
+    setMyProjects([...projects]);
+  }, []);
   return (
-    <div id="projects" className={projects.container}>
+    <div id="projects" className={project.container}>
       <h1>my projects</h1>
-      <div className={projects.projects}>
-        <div className={projects.project}></div>
+      <div className={project.projects}>
+        {myProjects.map(({ id, name, imgs, link, imgNum }) => (
+          <div className={project.project} key={id}>
+            <h1>{name}</h1>
+            <img src={`/assets/${imgs[imgNum]}.png`} alt={name} />
+            <a target="_blank" rel="noopener noreferrer" href={link}>
+              {name}
+            </a>
+            <div className={project.sliders}>
+              <button
+                onClick={() =>
+                  setMyProjects([
+                    ...myProjects.map((pro) =>
+                      pro.id === id
+                        ? {
+                            id,
+                            name,
+                            imgs,
+                            link,
+                            imgNum: imgNum === imgs.length - 1 ? 0 : imgNum + 1,
+                          }
+                        : {
+                            id: pro.id,
+                            name: pro.name,
+                            imgs: pro.imgs,
+                            link: pro.link,
+                            imgNum: pro.imgNum,
+                          }
+                    ),
+                  ])
+                }
+              >
+                &gt;
+              </button>
+              <button
+                onClick={() =>
+                  setMyProjects([
+                    ...myProjects.map((pro) =>
+                      pro.id === id
+                        ? {
+                            id,
+                            name,
+                            imgs,
+                            link,
+                            imgNum: imgNum === imgs.length - 1 ? 0 : imgNum + 1,
+                          }
+                        : {
+                            id: pro.id,
+                            name: pro.name,
+                            imgs: pro.imgs,
+                            link: pro.link,
+                            imgNum: pro.imgNum,
+                          }
+                    ),
+                  ])
+                }
+              >
+                &lt;
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
 export default Projects;
-
---
+++ src/data.ts
@@ -0,0 +1,47 @@
+export interface IProject {
+  id: number;
+  name: string;
+  imgs: string[];
+  link: string;
+  imgNum: number;
+}
+
+export const projects: IProject[] = [
+  {
+    id: 1,
+    name: "student app",
+    imgs: ["student-app-home", "student-app-student", "student-app-teacher"],
+    link: "https://stundent-app.netlify.app/",
+    imgNum: 0,
+  },
+  {
+    id: 2,
+    name: "cloth shop",
+    imgs: [
+      "cloth-shop-home",
+      "cloth-shop-men",
+      "cloth-shop-register",
+      "cloth-shop-dashboard",
+      "cloth-shop-cart-page",
+      "cloth-shop-product-details",
+      "cloth-shop-dashboard-admin",
+      "cloth-shop-banned-user",
+      "cloth-shop-edit-users",
+      "cloth-shop-register",
+    ],
+    link: "https://vivek80801-cloth-shop.netlify.app/",
+    imgNum: 0,
+  },
+  {
+    id: 3,
+    name: "ecommarce",
+    imgs: [
+      "vishavkarma-foods-home",
+      "vishawkarma-products",
+      "vishawkarma-details",
+      "vishawkarma-cart-page",
+    ],
+    link: "https://vishavkarma-foods.netlify.app/",
+    imgNum: 0,
+  },
+];
